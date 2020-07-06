c71307b5f9cf13012aab256c61cb702e3afde724
Jeong-hansol <jhs486674@gmail.com>
2020-07-06 15:09:27 +0900

    Create header nav and menubox js file

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(".nav>ul>li").mouseover(function(){
+	$(this).children(".menu_box").stop().slideDown();
+})
+$(".nav>ul>li").mouseleave(function(){
+	$(this).children(".menu_box").stop().slideUp();
+})
+
+
