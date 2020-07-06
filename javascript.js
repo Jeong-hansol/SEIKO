f5b4311360e640905565d8a18c7122a7bc3c6fa1
Jeong-hansol <jhs486674@gmail.com>
2020-07-06 15:28:05 +0900

    이미지 슬라이드 js

--- javascript.js
+++ javascript.js
@@ -5,4 +5,17 @@ $(".nav>ul>li").mouseleave(function(){
 	$(this).children(".menu_box").stop().slideUp();
 })
 
+start();
+var imgs=3;
+var now=0;
 
+function start(){
+	$(".imgs>img").eq(0).siblings().css({"margin-left":"-1600px"});
+	setInterval(function(){slide();},3000);
+}
+
+function slide(){
+	now=now==imgs?0:now+=1;
+	$(".imgs>img").eq(now-1).css({"margin-left":"-1600px"});
+	$(".imgs>img").eq(now).css({"margin-left":"0px"});
+}
