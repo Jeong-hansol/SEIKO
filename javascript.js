b4c994a9072a0625bda0d8e4e94e8e66dc0b5cb4
Jeong-hansol <jhs486674@gmail.com>
2020-07-10 18:27:49 +0900

    SEIKO US 이미지 클릭 시 Modal 창 추가 js

--- javascript.js
+++ javascript.js
@@ -19,3 +19,31 @@ function slide(){
 	$(".imgs>img").eq(now-1).css({"margin-left":"-1600px"});
 	$(".imgs>img").eq(now).css({"margin-left":"0px"});
 }
+
+$(".us_1").click(function(){
+	$("#modal").addClass("active");
+})
+$("#modal button").click(function(){
+	$("#modal").removeClass("active");
+})
+
+$(".us_2").click(function(){
+	$("#modal_2").addClass("active");
+})
+$("#modal_2 button").click(function(){
+	$("#modal_2").removeClass("active");
+})
+
+$(".us_3").click(function(){
+	$("#modal_3").addClass("active");
+})
+$("#modal_3 button").click(function(){
+	$("#modal_3").removeClass("active");
+})
+
+$(".us_4").click(function(){
+	$("#modal_4").addClass("active");
+})
+$("#modal_4 button").click(function(){
+	$("#modal_4").removeClass("active");
+})
